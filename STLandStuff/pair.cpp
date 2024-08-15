103d800fa9268aeca71595dcd7934597e9894473
still-nikk <nmaheshwari2904@gmail.com>
2024-08-15 15:21:31 +0530

    created STL folder and added pairs in STL

---
+++ STLandStuff/pair.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    pair<int, int> p = {1, 3};
+    cout << p.first << " " << p.second << endl;
+
+    pair<int, pair<int, int>> p2 = {1, {2, 3}};
+    cout << p2.first << " " << p2.second.first << " " << p2.second.second << endl;
+
+    return 0;
+}

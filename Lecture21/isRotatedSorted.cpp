eedeabdf576ad3442307eb2b7d81d50b530335fc
still-nikk <nmaheshwari2904@gmail.com>
2024-08-17 12:13:51 +0530

    optimizedLC1752

--- Lecture21/isRotatedSorted.cpp
+++ Lecture21/isRotatedSorted.cpp
@@ -3,33 +3,47 @@ using namespace std;
 
 bool check(vector<int> &nums)
 {
-    int k = nums.size();
-    while (k > 0)
+    // int k = nums.size();
+    // while (k > 0)
+    // {
+    //     bool isRotated = true;
+    //     vector<int> temp(nums.size());
+    //     for (int i = 0; i < nums.size(); i++)
+    //     {
+    //         temp[(i + k) % nums.size()] = nums[i];
+    //     }
+
+    //     for (int i = 0; i < temp.size() - 1; i++)
+    //     {
+    //         if (temp[i + 1] < temp[i])
+    //         {
+    //             isRotated = false;
+    //             break;
+    //         }
+    //     }
+
+    //     if (isRotated)
+    //     {
+    //         return true;
+    //     }
+
+    //     k--;
+    // }
+    // return false;
+
+    int count = 0;
+    for (int i = 0; i < nums.size() - 1; i++)
     {
-        bool isRotated = true;
-        vector<int> temp(nums.size());
-        for (int i = 0; i < nums.size(); i++)
+        if (nums[i + 1] < nums[i])
         {
-            temp[(i + k) % nums.size()] = nums[i];
+            count++;
         }
-
-        for (int i = 0; i < temp.size() - 1; i++)
-        {
-            if (temp[i + 1] < temp[i])
-            {
-                isRotated = false;
-                break;
-            }
-        }
-
-        if (isRotated)
-        {
-            return true;
-        }
-
-        k--;
     }
-    return false;
+    if (nums[nums.size() - 1] > nums[0])
+    {
+        count++;
+    }
+    return count <= 1;
 }
 
 int main()

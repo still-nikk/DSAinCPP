a9c632a356fd44d4dcca87fcc176070b657811ea
still-nikk <nmaheshwari2904@gmail.com>
2024-08-25 23:10:41 +0530

    permutationInString

---
+++ Lecture22/permutationInString.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// s2 contains the permutation of s1
+//  bool checkInclusion (string s1, string s2) {
+//  }
+
+int main()
+{
+
+    vector<int> arr(26);
+
+    cout << arr[5] + 1;
+
+    return 0;
+}
